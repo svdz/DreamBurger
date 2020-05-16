bbae84c9f5b631a017a15bfad3ba45463fc18ee7
Serhii <serg@gspam.com>
2020-05-16 12:27:19 +0300

    Working Burger button and out-click modal close.

--- script/script.js
+++ script/script.js
@@ -3,9 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const btnOpenModal = document.querySelector('#btnOpenModal');
     const modalBlock = document.querySelector('#modalBlock');
+    // const modalWrap = document.querySelector('.modal');
     const closeModal = document.querySelector('#closeModal');
     const questionTitle = document.querySelector('#question');
     const formAnswers = document.querySelector('#formAnswers');
+    const burgerBtn = document.getElementById('burger');
+    burgerBtn.style.display = "none";
+
+    let clientWidth = document.documentElement.clientWidth;
+    // console.dir('document.documentElement: ', document.documentElement);
+
+    if(clientWidth < 768) {
+        burgerBtn.style.display = 'flex';
+    } else {
+        burgerBtn.style.display = 'none';
+    }
 
     const playTest = () => {
         const renderQuestions = () => {
@@ -39,13 +51,53 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     btnOpenModal.addEventListener('click', () => {
-        console.log(': ', btnOpenModal);
+        // console.log(': ', btnOpenModal);
+        modalBlock.classList.add('d-block');
+        burgerBtn.classList.add('active');
+        playTest();
+    });
+
+    window.addEventListener('resize', () => {
+        clientWidth = document.documentElement.clientWidth;
+        // console.log(clientWidth);
+        if (clientWidth < 768) {
+            burgerBtn.style.display = 'flex';
+        } else {
+            burgerBtn.style.display = 'none';
+        }
+    });
+
+    burgerBtn.addEventListener('click', () => {
+        // if (burgerBtn.classList.contains('active')) {
+        //     burgerBtn.classList.remove('active');
+        // } else {
+        //     burgerBtn.classList.add('active');
+        // }
+
+        burgerBtn.classList.add('active');
         modalBlock.classList.add('d-block');
         playTest();
     });
 
     closeModal.addEventListener('click', () => {
         modalBlock.classList.remove('d-block');
+        burgerBtn.classList.remove('active');
     });
+
+    document.addEventListener('click', (event) => {
+        // console.log('event: ', event.target.closest('.openModalButton'));
+
+        if(
+        !event.target.closest('.modal-dialog') &&
+        !event.target.closest('.openModalButton') && 
+        !event.target.closest('.burger')) {
+            modalBlock.classList.remove('d-block');
+            burgerBtn.classList.remove('active');
+        }
+    });
+
+    // modalWrap.addEventListener('click', () => {
+    //     modalBlock.classList.remove('d-block');
+    // }); //buggy solution
 
 });
